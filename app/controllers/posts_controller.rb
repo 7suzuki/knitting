c3f9d392a1a9de76b1023145107cddc35eb82e8f
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-06-29 08:06:43 +0000

    [Add]post_index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @user = current_user
     # キーワード検索
     if params[:body].present?
-      @posts = @posts.where(body: params[:body])# 完全一致
-      # 曖昧検索もwhereを使う
+      @posts = @posts.where(body: params[:body]) # 完全一致
+      # @posts = @posts.where("body LIKE ?", "%#{params[:body]}%") # 曖昧検索もwhereを使う
     end
   end
 
