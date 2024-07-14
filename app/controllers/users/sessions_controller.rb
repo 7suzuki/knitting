5deab45da70c5d660a584f74b01cb79ba81556b1
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-07-14 12:05:42 +0000

    [Add]Unsubscribed

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :user_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -19,6 +19,22 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # protected
+
+  private
+  # アクティブであるかを判断するメソッド
+  def user_state
+    # 【処理内容1】 入力されたemailからアカウントを1件取得
+    user = User.find_by(name: params[:user][:name])
+    # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
+    return if user.nil?
+    # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+    return unless user.valid_password?(params[:user][:password])
+    # 【処理内容4】 アクティブでない会員に対する処理
+    if user.is_active == false
+      flash[:alert] = "このアカウントは退会済みです。"
+      redirect_to root_path and return
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:mypage]
-  
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -11,39 +11,38 @@ class UsersController < ApplicationController
     @posts = @user.posts
     render :show
   end
-  
+
   def edit
     @user = User.find(params[:id])
     unless @user.id == current_user.id
       redirect_to user_path
     end
   end
-  
+
   def update
     @user = User.find(params[:id])
-    
+
     if @user.id != current_user.id
       redirect_to user_path(current_user) and return
     end
-    
+
     if @user.update(user_params)
       redirect_to mypage_users_path notice: "プロフィールが更新されました"
     else
       render :edit, alert: "プロフィールの更新に失敗しました"
     end
   end
-  
-  def destroy　#editに入れたほうがいいかも？
+
+  def destroy
     @user = current_user
-    user.update!(is_active: false)
-    redirect_to user_path, notice: "退会しました"
+    @user.update!(is_active: false)
+    reset_session #ログアウトさせる
+    redirect_to root_path, notice: "退会しました"
   end
-  
-  private
-  
+
   def user_params
     params.require(:user).permit(:name, :comment)
   end
-  
+
 end
 
