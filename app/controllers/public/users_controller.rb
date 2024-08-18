229b095c5201bf3231df4b52d6a6e59c8eca306f
7suzuki <162399385+7suzuki@users.noreply.github.com>
2024-08-18 09:35:21 +0000

    [Add]guest_comment

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,6 @@
 class Public::UsersController < PublicController
   before_action :authenticate_user!, only: [:mypage]
-   before_action :ensure_guest_user, only: [:edit]
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
     @user = User.find(params[:id])
